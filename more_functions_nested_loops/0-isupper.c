b8ef8778ebb351e033813b47d9150365dd369a91
ArmandoMarrero <cmdnon9@gmail.com>
2023-10-19 10:22:00 -0400

    task0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 int _isupper(int c) {
     if (c >= 'A' && c <= 'Z') {
-        return 1; // Character is uppercase
+        return 1; 
     } else {
-        return 0; // Character is not uppercase
+        return 0; 
     }
 }
 
 int main() {
-    char testChar = 'H'; // You can change this character to test different cases
+    char testChar = 'H'; 
     if (_isupper(testChar)) {
         printf("%c is an uppercase character.\n", testChar);
     } else {
