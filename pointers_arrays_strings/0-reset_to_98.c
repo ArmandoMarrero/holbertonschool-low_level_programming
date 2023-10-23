6bb57f14016b6a4270adb693bcc02110a7a5c266
ArmandoMarrero <cmdnon9@gmail.com>
2023-10-23 13:21:12 -0400

    {taks0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,21 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
 
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter and
- * updates the value it points to to 98
- * @n: the integer adresse
+ * main - check the code 
  *
- * Return: 1 or 0
+ * Return: Always 0.
  */
-
-void reset_to_98(int *n)
+int main(void)
 {
-	*n = 98;
+  void updateValue(int *n)
+    *n = 98;
+  
+    
+
+    n = 98;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
 }
