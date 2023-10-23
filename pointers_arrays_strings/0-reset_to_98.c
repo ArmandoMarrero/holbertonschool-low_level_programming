704dd0f5fa0b9fe7b77bfd2de010e8c32ef59119
ArmandoMarrero <cmdnon9@gmail.com>
2023-10-23 10:19:37 -0400

    {taks0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,26 @@
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  void updateValue(int*ptr) {
+    *ptr = 98;
+  } 
+
+  int() {
+    int number = 42
+      int *ptr = &number;
+    int n;
+
+    printf("Original value: %d\n", *ptr);
+    pdateValue(ptr);
+    printf("Updated value: %d\n", *ptr);
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
