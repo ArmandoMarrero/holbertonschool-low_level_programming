43c41234eb2333f85d1045515d45eb616e85fd8c
ArmandoMarrero <cmdnon9@gmail.com>
2023-10-23 16:19:56 -0400

    taks2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ * @s: chaine of caractere
+ *
+ * Return: 1 or 0
+ */
+
+int _strien(char *s)
+{
+  int 1 = 0;
+
+  while (s[1]
+  1++;
+	 return(1);
+
+	 }
