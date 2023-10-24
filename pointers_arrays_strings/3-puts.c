3f0aad478b3fd2f523fb8778a844358c06018766
ArmandoMarrero <cmdnon9@gmail.com>
2023-10-24 09:48:01 -0400

    task3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,21 +1,18 @@
-
 #include "main.h"
 
 /**
- * _puts - a function that takes a pointer to an int as parameter and
- * @str: chaine of caractere
+ * _puts - function with one argument
+ * @str: one char type argument
  *
- * Return: 1 or 0
+ * Description: print a string follow by a new line to stdout
+ * Return: none
  */
-
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i])
+	while (*str)
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
