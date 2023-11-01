b3dd8e1fe79296d63b0fa78e6b5502577d3ec2fd
ArmandoMarrero <cmdnon9@gmail.com>
2023-11-01 09:10:52 -0400

    task0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line
- * @s: string
+ * _puts_recursion - function with 1 argument
+ * @s: char type pointer
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Description: prints a string follow by a new line
+ * Return: na
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
